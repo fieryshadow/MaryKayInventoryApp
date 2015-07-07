59cf0b80a458c0fadeb7f7cf69efc660b512a97b
harstong <har11057@byui.edu>
2015-07-06 20:20:53 -0600

    database monster connected to app

--- app/src/main/java/edu/byui/shane/marykayinventoryapp/InventoryManager.java
+++ app/src/main/java/edu/byui/shane/marykayinventoryapp/InventoryManager.java
@@ -1,5 +1,6 @@
 package edu.byui.shane.marykayinventoryapp;
 
+import android.content.Context;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -11,13 +12,24 @@ import java.util.List;
  * what's in the inventory, out of stock, on order, etc.
  */
 public class InventoryManager {
+    private static Context appContext;
     private Hashtable<String, ProductEntry> inventory; // barcode: ProductEntry
 
-    private static final InventoryManager manager = new InventoryManager();
-    public static InventoryManager getInstance() { return manager; } // singleton for use in app
-
+    private static InventoryManager manager; // final...
     private InventoryManager() {
         inventory = new Hashtable<>();
+    }
+    public static InventoryManager getInstance() {
+        Log.i(MainActivity.TAG_FOR_APP, "here now");
+        return manager;
+    } // singleton for use in app
+
+    public static void createSingleton(Context context) {
+        if (manager == null) {
+            Log.i(MainActivity.TAG_FOR_APP, "got here");
+            appContext = context;
+            manager = new InventoryManager();
+        }
     }
 
     public List<ProductInfo> getListing() {
@@ -69,19 +81,24 @@ public class InventoryManager {
         if (inventory.containsKey(productKey)) {
             Log.i(MainActivity.TAG_FOR_APP, "adding " + numOfProduct + " to " + inventory.get(productKey).getInfo().getId());
             inventory.get(productKey).setNumberInStock(inventory.get(productKey).getNumberInStock() + numOfProduct);
+            ProductDataSource.getInstance().storeProduct(inventory.get(productKey));
             Log.i(MainActivity.TAG_FOR_APP, "total in stock = " + inventory.get(productKey).getNumberInStock()+numOfProduct);
         }
         else {
             Log.i(MainActivity.TAG_FOR_APP, "Adding a new product");
-            inventory.put(createProductKey(productNumber, section), new ProductEntry(new Product(productNumber, category, name, section, color, cost), numOfProduct, 0, numOfProduct));
+            ProductEntry productEntry = new ProductEntry(new Product(productNumber, category, name, section, color, cost), numOfProduct, 0, numOfProduct);
+            inventory.put(createProductKey(productNumber, section), productEntry);
+            Log.i(MainActivity.TAG_FOR_APP, "data time");
+            ProductDataSource.getInstance().storeProduct(productEntry);
             Log.i(MainActivity.TAG_FOR_APP, "Done adding new Product");
         }
 
     }
 
     /**
-     * Removes the product from the inventory list based off of the productKey
+     * Removes the product from the inventory list based off of the productKey and how much of the product to remove.
      * @param productKey The product identifier
+     * @param numProduct the number of product that is being removed.
      */
     public void removeProduct(String productKey, int numProduct) {
         inventory.get(productKey).setNumberInStock(inventory.get(productKey).getNumberInStock()-numProduct);
@@ -94,7 +111,7 @@ public class InventoryManager {
      * @param section Is it a sample product or actual retail
      * @return Returns a unique identifier taking into account the product section
      */
-    private String createProductKey(String productNumber, String section) {
+    public static String createProductKey(String productNumber, String section) {
         return productNumber + section;
     }
 
@@ -116,7 +133,11 @@ public class InventoryManager {
      * create the local inventory list from the database that has the inventory list stored.
      */
     public void readFromDatabase() {
-        inventory.put(createProductKey("1234", "A"), new ProductEntry(new Product("1234", "foundation", "liquid", "A", "peach", 12.34f), 0, 0, 0));
+        Log.i(MainActivity.TAG_FOR_APP, "where are you");
+        inventory.putAll(ProductDataSource.getInstance().readAllProducts());
+        Log.i(MainActivity.TAG_FOR_APP, "done now");
+
+        /*inventory.put(createProductKey("1234", "A"), new ProductEntry(new Product("1234", "foundation", "liquid", "A", "peach", 12.34f), 0, 0, 0));
         inventory.put(createProductKey("5678", "A"), new ProductEntry(new Product("5678", "foundation", "liquid", "A", "dark", 34.34f), 4, 5, 6));
         inventory.put(createProductKey("2345", "B"), new ProductEntry(new Product("2435", "eyeliner", "lash enhancing", "B", "deep black", 56.34f), 7, 8, 9));
         inventory.put(createProductKey("3456", "A"), new ProductEntry(new Product("3456", "lipstick", "liquid", "A", "bright red", 78.34f), 0, 0, 0));
@@ -125,6 +146,6 @@ public class InventoryManager {
         inventory.put(createProductKey("9876", "A"), new ProductEntry(new Product("9876", "foundation", "liquid", "A", "light", 34.34f), 4, 5, 6));
         inventory.put(createProductKey("8765", "B"), new ProductEntry(new Product("8765", "eyeliner", "lash enhancing", "B", "pale sparkles", 56.34f), 7, 8, 9));
         inventory.put(createProductKey("7654", "A"), new ProductEntry(new Product("7654", "lipstick", "liquid", "A", "leafy", 78.34f), 0, 0, 0));
-        inventory.put(createProductKey("6543", "B"), new ProductEntry(new Product("6543", "blush", "gnarly", "B", "soft", 96.57f), 3, 5, 7));
+        inventory.put(createProductKey("6543", "B"), new ProductEntry(new Product("6543", "blush", "gnarly", "B", "soft", 96.57f), 3, 5, 7));//*/
     }
 }
